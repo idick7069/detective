30a98b3e134c3e0ed383f520a163cad3060b349b
Frank <a10423028@yuntech.org.tw>
2022-01-06 01:44:40 +0800

    set dialog responsive

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -5,12 +5,30 @@
     <!-- <v-container fluid> -->
     <router-view></router-view>
     <div>
-      <v-btn class="fixedContainer" dark bottom right height="100" width="100" color="red" variant="default">
+      <v-btn
+        class="fixedContainer"
+        dark
+        bottom
+        right
+        height="100"
+        width="100"
+        color="red"
+        variant="default"
+      >
         <div v-html="inputValue"></div>
-        <v-dialog v-model="dialog" activator="parent" fullscreen>
+        <v-dialog v-model="dialog" activator="parent" fullscreen="true">
           <div class="dialogBg" v-html="dialogBgHtml"></div>
           <div class="dialogContent" v-html="dialogContentHtml"></div>
-          <v-btn flat color="transparent" width="80" height="80" class="closeBtn" position="absolute" right top>
+          <v-btn
+            flat
+            color="transparent"
+            width="80"
+            height="80"
+            class="closeBtn"
+            position="absolute"
+            right
+            top
+          >
             <v-img :src="close_icon" @click="closeDialog()"></v-img>
           </v-btn>
         </v-dialog>
@@ -22,104 +40,208 @@
 
 
 <script>
-  import manual from "../assets/manual.svg?raw";
-  import manual_item_1 from "../assets/manual_item_1.svg?raw";
-  import scale_form from "../assets/scale_form.svg?raw";
-  import scale_content from "../assets/scale_content.svg?raw";
-  import scale_icon from "../assets/icon/scale_icon.svg?url";
+import manual from "../assets/dialog/manual_bg.svg?raw";
+import manual_item_1 from "../assets/dialog/manual_content.svg?raw";
+import scale_form from "../assets/dialog/scale_bg.svg?raw";
+import scale_content from "../assets/dialog/scale_content.svg?raw";
+import scale_icon from "../assets/icon/scale_icon.svg?url";
 
-  import close_icon from "../assets/icon/close.png"
-  export default {
-    name: "Container",
-    data: () => ({
-      dialog: false,
-      manual,
-      manual_item_1,
-      scale_form,
-      scale_content,
-      close_icon,
-      inputValue: '<img width="100" height="100" src="' + scale_icon + '"></img>',
-      dialogBgHtml: scale_form,
-      dialogContentHtml: scale_content,
-    }),
-    mounted() {
-      this.$store.commit(
-        "setFloatingTemplate",
-        '<img  width="100" height="100" src="' + scale_icon + '"></img>'
-      );
+import close_icon from "../assets/icon/close.png";
+export default {
+  name: "Container",
+  data: () => ({
+    dialog: false,
+    manual,
+    manual_item_1,
+    scale_form,
+    scale_content,
+    close_icon,
+    inputValue: '<img width="100" height="100" src="' + scale_icon + '"></img>',
+    dialogBgHtml: scale_form,
+    dialogContentHtml: scale_content,
+  }),
+  mounted() {
+    this.$store.commit(
+      "setFloatingTemplate",
+      '<img  width="100" height="100" src="' + scale_icon + '"></img>'
+    );
+    this.inputValue = this.$store.state.floatingTemplate;
+
+    this.$store.commit("setFloatingDialog", true);
+    this.$store.commit("setDialogOpen", false);
+    this.dialog = this.$store.state.dialogOpen;
+  },
+  watch: {
+    "$store.state.floatingTemplate": function (newVal) {
       this.inputValue = this.$store.state.floatingTemplate;
-
-      this.$store.commit("setFloatingDialog", true);
-      this.$store.commit("setDialogOpen", false);
-      this.dialog = this.$store.state.dialogOpen;
     },
-    watch: {
-      "$store.state.floatingTemplate": function (newVal) {
-        this.inputValue = this.$store.state.floatingTemplate;
-      },
-      "$store.state.floatingDialog": function (newVal) {
-        if ((newVal == false)) {
-          this.dialogBgHtml = manual;
-          this.dialogContentHtml = manual_item_1;
-        } else {
-          this.dialogBgHtml = scale_form;
-          this.dialogContentHtml = scale_content;
-        }
-      },
-      "$store.state.dialogOpen": function (newVal) {
-        this.dialog = this.$store.state.dialogOpen;
-      },
-    },
-    methods: {
-      closeDialog() {
-        this.dialog = false
-        this.$store.commit("setDialogOpen", false);
+    "$store.state.floatingDialog": function (newVal) {
+      if (newVal == false) {
+        this.dialogBgHtml = manual;
+        this.dialogContentHtml = manual_item_1;
+      } else {
+        this.dialogBgHtml = scale_form;
+        this.dialogContentHtml = scale_content;
       }
     },
-  };
+    "$store.state.dialogOpen": function (newVal) {
+      this.dialog = this.$store.state.dialogOpen;
+    },
+    dialog: function () {
+      console.log("dialog狀態改變");
+      //  this.resizeSvg()
+      window.setTimeout(() => {
+        console.log("Hello!");
+        this.resizeSvg();
+      }, 500);
+    },
+  },
+  methods: {
+    closeDialog() {
+      this.dialog = false;
+      this.$store.commit("setDialogOpen", false);
+    },
+    resizeChild(myElement, widthScale, heightScale) {
+      console.log("resize child");
+
+      for (let i = 0; i < myElement.children.length; i++) {
+        var reSizeWidth =
+          myElement.children[i].getAttribute("width") * widthScale;
+        var reSizeHeight =
+          myElement.children[i].getAttribute("height") * heightScale;
+        myElement.children[i].setAttribute("width", reSizeWidth);
+        myElement.children[i].setAttribute("height", reSizeHeight);
+        var originalx = myElement.children[i].getAttribute("x");
+        if (originalx != null) {
+          myElement.children[i].setAttribute("x", originalx * widthScale);
+        }
+        var originaly = myElement.children[i].getAttribute("y");
+        if (originaly != null) {
+          myElement.children[i].setAttribute("y", originaly * heightScale);
+        }
+      }
+    },
+    resizeSvg() {
+      console.log("resize");
+
+      var widthScale = window.screen.width / 1920.0;
+      var heightScale = (window.screen.width * 0.5625) / 1080.0;
+      var containerWidth = window.screen.width;
+      var containerHeight = window.screen.width * 0.5625;
+      console.log(widthScale);
+      console.log(heightScale);
+      console.log(containerWidth);
+      console.log(containerHeight);
+      console.log("測試" + document.getElementById("scale-bg"));
+      if (document.getElementById("manual-bg") != null) {
+        document
+          .getElementById("manual-bg")
+          .setAttribute("width", containerWidth);
+        document
+          .getElementById("manual-bg")
+          .setAttribute("height", containerHeight);
+        document
+          .getElementById("manual-content")
+          .setAttribute("width", containerWidth);
+        document
+          .getElementById("manual-content")
+          .setAttribute("height", containerHeight);
+
+        document
+          .getElementById("manual-bg")
+          .setAttribute(
+            "viewBox",
+            "0 0 " + containerWidth + " " + containerHeight
+          );
+        document
+          .getElementById("manual-content")
+          .setAttribute(
+            "viewBox",
+            "0 0 " + containerWidth + " " + containerHeight
+          );
+
+        var myElement = document.getElementById("manual-bg");
+        this.resizeChild(myElement, widthScale, heightScale);
+        myElement = document.getElementById("manual-content");
+        this.resizeChild(myElement, widthScale, heightScale);
+      } else if (document.getElementById("scale-bg") != null) {
+        document
+          .getElementById("scale-bg")
+          .setAttribute("width", containerWidth);
+        document
+          .getElementById("scale-bg")
+          .setAttribute("height", containerHeight);
+        document
+          .getElementById("scale-content")
+          .setAttribute("width", containerWidth);
+        document
+          .getElementById("scale-content")
+          .setAttribute("height", containerHeight);
+
+        document
+          .getElementById("scale-bg")
+          .setAttribute(
+            "viewBox",
+            "0 0 " + containerWidth + " " + containerHeight
+          );
+        document
+          .getElementById("scale-content")
+          .setAttribute(
+            "viewBox",
+            "0 0 " + containerWidth + " " + containerHeight
+          );
+
+        var myElement2 = document.getElementById("scale-bg");
+        this.resizeChild(myElement2, widthScale, heightScale);
+        myElement2 = document.getElementById("scale-content");
+        this.resizeChild(myElement2, widthScale, heightScale);
+      }
+    },
+  },
+};
 </script>
 <style scoped>
-  @media (min-width: 1920px) {
-    .v-container {
-      max-width: 100vw;
-      padding: 0px;
-    }
+@media (min-width: 1920px) {
+  .v-container {
+    max-width: 100vw;
+    padding: 0px;
   }
+}
 
-  @media (min-width: 1px) {
-    .v-container {
-      max-width: 100vw;
-      padding: 0px;
-    }
+@media (min-width: 1px) {
+  .v-container {
+    max-width: 100vw;
+    padding: 0px;
   }
+}
 
+.fixedContainer {
+  position: fixed;
+  transform: translate(-10%, -10%);
+  z-index: 2;
+}
+
+@media (orientation: portrait) {
   .fixedContainer {
-    position: fixed;
-    transform: translate(-10%, -10%);
-    z-index: 2;
+    z-index: -1;
   }
+}
 
-  @media (orientation: portrait) {
-    .fixedContainer {
-      z-index: -1;
-    }
-  }
+.dialogBg {
+  position: absolute;
+}
 
-  .dialogBg {
-    position: absolute;
-  }
+.dialogContent {
+  pointer-events: none;
+  position: absolute;
+}
 
-  .dialogContent {
-    pointer-events: none;
-    position: absolute;
-  }
-
-  .closeBtn {
-    transform: translate(-10px, 10px);
-  }
+.closeBtn {
+  transform: translate(-10px, -10px);
+}
 </style>
 <style>
-  [data-name="red_tag"] {
-    pointer-events: auto;
-  }
+[data-name="red_tag"] {
+  pointer-events: auto;
+}
 </style>
--- src/components/Stomach.vue
+++ src/components/Stomach.vue
@@ -1,129 +1,193 @@
 <template>
   <!-- <v-container> -->
-  <div ref="responsive" style="width:100vw;height:56.25vw">
-
-
+  <div ref="responsive" style="width: 100vw; height: 56.25vw">
     <div class="stomach_background" v-html="stomachBackground"></div>
     <div class="stomach" v-html="stomach" @click="onClick($event)"></div>
+    
   </div>
-  <!-- <v-img class="stomach_hint" :src="stomachHint" @click="isActive = true"
-      v-bind:style='{"display": (isActive? "none" : "block" )}'></v-img> -->
-  <v-btn flat color="rgba(220, 220, 220, 0.8)" width="180" height="180">
-    <v-img :src="logoutIcon" @click="leavePage()" position="0 0"></v-img>
-  </v-btn>
-
+  <div class="logout">
+      <v-btn flat color="rgba(220, 220, 220, 0.8)" width="180" height="180">
+        <v-img :src="logoutIcon" @click="leavePage()" position="0 0"></v-img>
+      </v-btn>
+    </div>
+  <img class="stomach_hint" id="stomach_hint" :src="stomachHint" @click="isActive = true"
+      v-bind:style='{"display": (isActive? "none" : "block" )}'/>
 
   <!-- </v-container> -->
 </template>
 
 <script>
-  import stomach from "../assets/stomach_scene/stomach_obj.svg?raw";
-  import stomachBackground from "../assets/stomach_scene/stomach_bg.svg?raw";
-  import stomachHint from "../assets/stomach_scene/stomach_hint.png"
-  import cursorIcon from "../assets/cursor/magnifier_cursor.png";
-  import manualIcon from "../assets/icon/manual_icon.svg?url";
-  import scaleIcon from "../assets/icon/scale_icon.svg?url";
-  import logoutIcon from "../assets/icon/logout.png"
+import stomach from "../assets/stomach_scene/stomach_obj.svg?raw";
+import stomachBackground from "../assets/stomach_scene/stomach_bg.svg?raw";
+import stomachHint from "../assets/stomach_scene/stomach_hint.png";
+import cursorIcon from "../assets/cursor/magnifier_cursor.png";
+import manualIcon from "../assets/icon/manual_icon.svg?url";
+import scaleIcon from "../assets/icon/scale_icon.svg?url";
+import logoutIcon from "../assets/icon/logout.png";
 
-  export default {
-    name: "Stomach",
-    data: () => ({
-      stomach,
-      stomachBackground,
-      cursorIcon,
-      stomachHint,
-      logoutIcon,
-      selectedCursor: `url("` + cursorIcon + `") 2 2, help`,
-      isActive: false
-    }),
-    methods: {
-      leavePage() {
-        this.$router.go(-1)
-      },
-      onClick(event) {
-
-        let targetId = event.target.id
-        console.log(targetId)
-        if (targetId == 'cigarette') {
-          this.$store.commit("setDialogOpen", true);
-        }
-      },
-      resizeChild(myElement, widthScale, heightScale) {
-
-        for (let i = 0; i < myElement.children.length; i++) {
-          var reSizeWidth = myElement.children[i].getAttribute("width") * widthScale
-          var reSizeHeight = myElement.children[i].getAttribute("height") * heightScale
-          myElement.children[i].setAttribute("width", reSizeWidth);
-          myElement.children[i].setAttribute("height", reSizeHeight);
-          var originalx = myElement.children[i].getAttribute("x")
-          if (originalx != null) {
-            myElement.children[i].setAttribute("x", originalx * widthScale);
-          }
-          var originaly = myElement.children[i].getAttribute("y")
-          if (originaly != null) {
-            myElement.children[i].setAttribute("y", originaly * heightScale);
-          }
-        }
-      },
-      resizeSvg() {
-        console.log("resize");
-
-        var widthScale = this.$refs.responsive.clientWidth / 1920.0
-        var heightScale = this.$refs.responsive.clientHeight / 1080.0
-        var containerHeight = this.$refs.responsive.clientHeight;
-        var containerWidth = this.$refs.responsive.clientWidth;
-        console.log(widthScale);
-        console.log(heightScale);
-        document.getElementById("in_cabinet").setAttribute("width", containerWidth);
-        document.getElementById("in_cabinet").setAttribute("height", containerHeight);
-
-        document.getElementById("in_cabinet_bg").setAttribute("width", containerWidth);
-        document.getElementById("in_cabinet_bg").setAttribute("height", containerHeight);
-
-        document.getElementById("in_cabinet_bg").setAttribute("viewBox", "0 0 " + containerWidth + " " +
-          containerHeight);
-        document.getElementById("in_cabinet").setAttribute("viewBox", "0 0 " + containerWidth + " " + containerHeight);
-
-        var myElement = document.getElementById('in_cabinet');
-
-        this.resizeChild(myElement, widthScale, heightScale);
-
-        myElement = document.getElementById('in_cabinet_bg');
-
-        this.resizeChild(myElement, widthScale, heightScale);
-
-      },
+export default {
+  name: "Stomach",
+  data: () => ({
+    stomach,
+    stomachBackground,
+    cursorIcon,
+    stomachHint,
+    logoutIcon,
+    selectedCursor: `url("` + cursorIcon + `") 2 2, help`,
+    isActive: false,
+    isResized: false,
+  }),
+  methods: {
+    leavePage() {
+      this.$router.go(-1);
     },
-    //onCreate
-    mounted() {
+    onClick(event) {
+      let targetId = event.target.id;
+      console.log(targetId);
+      if (targetId == "cigarette") {
+        this.$store.commit("setDialogOpen", true);
+      }
+    },
+    resizeChild(myElement, widthScale, heightScale) {
+      for (let i = 0; i < myElement.children.length; i++) {
+        var reSizeWidth =
+          myElement.children[i].getAttribute("width") * widthScale;
+        var reSizeHeight =
+          myElement.children[i].getAttribute("height") * heightScale;
+        myElement.children[i].setAttribute("width", reSizeWidth);
+        myElement.children[i].setAttribute("height", reSizeHeight);
+        var originalx = myElement.children[i].getAttribute("x");
+        if (originalx != null) {
+          myElement.children[i].setAttribute("x", originalx * widthScale);
+        }
+        var originaly = myElement.children[i].getAttribute("y");
+        if (originaly != null) {
+          myElement.children[i].setAttribute("y", originaly * heightScale);
+        }
+      }
+    },
+    resizeSvg() {
+      console.log("resize");
+
+      var widthScale = window.screen.width / 1920.0;
+      var heightScale = (window.screen.width * 0.5625) / 1080.0;
+      var containerWidth = window.screen.width;
+      var containerHeight = window.screen.width * 0.5625;
+      console.log(widthScale);
+      console.log(heightScale);
+       document
+        .getElementById("stomach_hint")
+        .setAttribute("width", containerWidth);
+      document
+        .getElementById("stomach_hint")
+        .setAttribute("height", containerHeight);
+
+      document
+        .getElementById("in_cabinet")
+        .setAttribute("width", containerWidth);
+      document
+        .getElementById("in_cabinet")
+        .setAttribute("height", containerHeight);
+
+      document
+        .getElementById("in_cabinet_bg")
+        .setAttribute("width", containerWidth);
+      document
+        .getElementById("in_cabinet_bg")
+        .setAttribute("height", containerHeight);
+
+      document
+        .getElementById("in_cabinet_bg")
+        .setAttribute(
+          "viewBox",
+          "0 0 " + containerWidth + " " + containerHeight
+        );
+      document
+        .getElementById("in_cabinet")
+        .setAttribute(
+          "viewBox",
+          "0 0 " + containerWidth + " " + containerHeight
+        );
+
+      var myElement = document.getElementById("in_cabinet");
+
+      this.resizeChild(myElement, widthScale, heightScale);
+
+      myElement = document.getElementById("in_cabinet_bg");
+
+      this.resizeChild(myElement, widthScale, heightScale);
+    },
+    handleOrientationChange() {
+      console.log("change");
+      const orientation = window.screen.orientation.type;
+      if (orientation === "portrait-primary") {
+        // portrait mode
+      } else if (orientation === "landscape-primary") {
+        var needResize = false;
+        if (
+          document.getElementById("in_cabinet_bg").getAttribute("width") == 1920
+        ) {
+          needResize = true;
+        }
+        if (needResize) {
+          this.isResized = true;
+          this.resizeSvg();
+        }
+        // landscape mode
+      }
+    },
+  },
+  //onCreate
+  mounted() {
+    // this.resizeSvg();
+    this.$store.commit(
+      "setFloatingTemplate",
+      '<img  width="100" height="100" src="' + manualIcon + '"></img>'
+    );
+    this.$store.commit("setFloatingDialog", false);
+
+    console.log("amounted");
+
+    const orientation = window.screen.orientation.type;
+
+    if (
+      document.getElementById("in_cabinet_bg").getAttribute("width") == 1920 &&
+      orientation === "landscape-primary"
+    ) {
+      this.needResize = true;
+    }
+    if (this.needResize) {
+      this.isResized = true;
       this.resizeSvg();
-      this.$store.commit(
-        "setFloatingTemplate",
-        '<img  width="100" height="100" src="' + manualIcon + '"></img>'
-      );
-      this.$store.commit("setFloatingDialog", false);
-    },
-    //onDestory
-    beforeUnmount() {
-      this.$store.commit(
-        "setFloatingTemplate",
-        '<img  width="100" height="100" src="' + scaleIcon + '"></img>'
-      );
-      this.$store.commit("setFloatingDialog", true);
-    },
-  };
+    }
+  },
+  //onDestory
+  beforeUnmount() {
+    this.$store.commit(
+      "setFloatingTemplate",
+      '<img  width="100" height="100" src="' + scaleIcon + '"></img>'
+    );
+    this.$store.commit("setFloatingDialog", true);
+  },
+};
 </script>
 <style>
-  .stomach_background,
-  .stomach_hint,
-  .stomach {
-    height: 56.25vw;
-    width: 100vw;
-    position: absolute;
-  }
+.stomach_background,
+.stomach {
+  position: absolute;
+}
+.stomach_hint{
+   top: 0;
+  left: 0;
+  position: absolute;
+}
 
-
-  [data-name="cigarette"] {
-    cursor: v-bind(selectedCursor);
-  }
+.logout {
+  top: 0;
+  left: 0;
+  position: absolute;
+}
+[data-name="cigarette"] {
+  cursor: v-bind(selectedCursor);
+}
 </style>
